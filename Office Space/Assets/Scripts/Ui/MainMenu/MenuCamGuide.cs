b80cb599aa589f0b0d1234081b2ddaab890dc20c
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-03-28 16:48:06 +0200

    Main Menu Basic
    keeping simple, still need to photoshop buttons and choose font so did not add much. Camera can be moved for when we add the office. Assets added text mesh pro, download it its really usefull

---
+++ Office Space/Assets/Scripts/Ui/MainMenu/MenuCamGuide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCamGuide : MonoBehaviour {
+
+	public Transform currentMount;
+
+	public float speedFactor= 0.1f;
+	public float zoomFactor= 1.0f;
+	public Vector3 lastPosition;
+
+	void  Start ()
+	{
+		lastPosition = transform.position;
+	}
+
+	void  Update ()
+	{
+		transform.position = Vector3.Lerp(transform.position, currentMount.position, speedFactor);
+		transform.rotation = Quaternion.Slerp(transform.rotation, currentMount.rotation, speedFactor);
+
+
+
+
+	}
+
+	public void  setMount ( Transform newMount  )
+	{
+		currentMount = newMount;
+	}﻿
+}
