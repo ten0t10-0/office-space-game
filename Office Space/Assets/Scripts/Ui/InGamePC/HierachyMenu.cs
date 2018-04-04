b0ec126233b8777a2ce6a4f70fefe7ff2301570f
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-04-04 20:23:36 +0200

    Basic Computer UI
    *super basic
    *in app menus are toggle on off, will change that
    *touch cube to open
    *forgot to add a close

---
+++ Office Space/Assets/Scripts/Ui/InGamePC/HierachyMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HierachyMenu : MonoBehaviour 
+
+{
+	public int tier = 0;
+	public bool shown = false;
+
+	public static List<HierachyMenu> menus = new List<HierachyMenu>();
+
+	public void Awake()
+	{
+		menus.Add (this);
+	}
+
+	public void OnDestroy()
+	{
+		menus.Remove (this);
+	}
+
+	public void Show(bool show)
+	{
+		if (show)
+		{
+			foreach (HierachyMenu menu in menus) 
+			{
+				if ((menu != this) && (menu.shown) && (menu.tier >= tier))
+					menu.Show (false);
+			}	
+		}
+
+				
+		shown = show;
+		gameObject.SetActive (shown);
+
+	}
+	public void Toggle()
+	{
+		Show (!shown);
+	}
+
+}
---
+++ Office Space/Assets/Scripts/Ui/InGamePC/PcTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PcTrigger : MonoBehaviour {
+
+	public Canvas ComputerCanvas;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("Player"));
+		OpenShop();
+			
+	}
+	void OpenShop()
+	{
+		ComputerCanvas.enabled = true;
+		Time.timeScale = 0;
+		Cursor.visible = (ComputerCanvas.gameObject.activeInHierarchy);
+	}
+
+	void CloseShop()
+	{
+		ComputerCanvas.enabled = false;
+		Time.timeScale = 1;
+	}
+
+
+}
