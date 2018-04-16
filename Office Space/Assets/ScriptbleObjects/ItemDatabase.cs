d095b5d0e7ac09d29f8101df4db136a44d49b5fe
B <36201345+ten0t10-0@users.noreply.github.com>
2018-04-17 00:59:04 +0200

    UI + SO to list *

---
+++ Office Space/Assets/ScriptbleObjects/ItemDatabase.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="ItemDatabase",menuName="Database")]
+public class ItemDatabase : ScriptableObject
+{
+
+	public List <ItemTemp> itemDictionary = new List<ItemTemp> ();
+}
