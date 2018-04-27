2dde6b3657175d6cfe3254fe5092da80d13dcc20
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-04-27 23:07:32 +0200

    itemList
    displays SO to scroll view
    need add sort by categorys
    not working atm

---
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/PopulateScrollView.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateScrollView : MonoBehaviour 
+{
+
+	ShopController shop = new ShopController ();
+
+	public void DefaultList()
+	{
+		shop.PopulateInventory ();
+	}
+
+
+}
---
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopController : MonoBehaviour 
+{
+	
+	[SerializeField]
+	private GameObject ItemContainer;
+	private Transform scrollViewContent;
+
+	SupplierManager sm = new SupplierManager();
+
+
+	public void PopulateInventory()
+	{
+
+		ClearInventory();
+		int i = 0;
+
+		foreach (SupplierAI supplier in sm.Suppliers)
+		{
+			i++;
+		
+			foreach( InventoryItem item in sm.Suppliers[i].Inventory.Items)
+			{
+				GameObject newItem = Instantiate(ItemContainer, scrollViewContent);
+
+				newItem.transform.localScale = Vector3.one;
+
+				newItem.transform.Find("Image").GetComponent<Image> ().sprite = item.GetItemSO().Picture;
+				newItem.transform.Find("Name").GetComponent<Text>().text = item.GetItemSO().Name;
+				newItem.transform.Find("Price").GetComponent<Text>().text = item.GetItemSO().UnitCost.ToString();
+
+
+			}
+		}
+	}
+
+
+	/// Clears out any existing inventory UI items
+	public void ClearInventory()
+	{
+		
+		if (scrollViewContent == null)
+		{
+			scrollViewContent = transform.Find("Scroll View/Viewport/Content");
+		}
+
+		foreach (Transform child in scrollViewContent)
+		{
+			Destroy(child.gameObject);
+		}
+	}
+
+}
