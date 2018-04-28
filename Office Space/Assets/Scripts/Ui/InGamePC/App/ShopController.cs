1db3ecb71eb0dc19b0f4fc3427fe7c2d47ef166c
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-04-28 21:06:09 +0200

    changed prefabs
     shop still not working ;/

--- Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
@@ -3,23 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopController 
+public class ShopController : MonoBehaviour 
 {
 	
+	[SerializeField]
+	private GameObject ItemContainer;
+	private Transform scrollViewContent;
+
+	
+
+
 	public void PopulateInventory()
 	{
+	
+		scrollViewContent = transform.Find("Scroll View/Viewport/Content");
 
-		foreach (SupplierAI s in GameMaster.Instance.SupplierManager.Suppliers)
-		{
+        foreach (SupplierAI s in GameMaster.Instance.SupplierManager.Suppliers)
+        {
 		
-				foreach (InventoryItem item in s.Inventory.Items)
+		
+			foreach (InventoryItem item in s.Inventory.Items)
 			{
-					Debug.Log(item.ToString());
+				GameObject newItem = Instantiate(ItemContainer,scrollViewContent);
+
+				newItem.transform.localScale = Vector3.one;
+
+				newItem.transform.Find("Image").GetComponent<Image> ().sprite = item.GetItemSO().Picture;
+				newItem.transform.Find("Name").GetComponent<Text>().text = item.GetItemSO().Name;
+				newItem.transform.Find("Price").GetComponent<Text>().text = item.GetItemSO().UnitCost.ToString();
 
 
 			}
 		}
 	}
-
+		
 
 }
