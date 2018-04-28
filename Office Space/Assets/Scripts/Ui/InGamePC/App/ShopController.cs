054737a1c120705dba5d6c0ff4e471f7dff46082
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-04-28 17:11:13 +0200

    shopTest

--- Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
@@ -6,52 +6,22 @@ using UnityEngine.UI;
 public class ShopController : MonoBehaviour 
 {
 	
-	[SerializeField]
-	private GameObject ItemContainer;
-	private Transform scrollViewContent;
-
 	SupplierManager sm = new SupplierManager();
-
 
 	public void PopulateInventory()
 	{
 
-		ClearInventory();
-		int i = 0;
-
-		foreach (SupplierAI supplier in sm.Suppliers)
+		foreach (SupplierAI s in sm.Suppliers)
 		{
-			i++;
 		
-			foreach( InventoryItem item in sm.Suppliers[i].Inventory.Items)
+				foreach (InventoryItem item in s.Inventory.Items)
 			{
-				GameObject newItem = Instantiate(ItemContainer, scrollViewContent);
-
-				newItem.transform.localScale = Vector3.one;
-
-				newItem.transform.Find("Image").GetComponent<Image> ().sprite = item.GetItemSO().Picture;
-				newItem.transform.Find("Name").GetComponent<Text>().text = item.GetItemSO().Name;
-				newItem.transform.Find("Price").GetComponent<Text>().text = item.GetItemSO().UnitCost.ToString();
+					Debug.Log(item.ToString());
 
 
 			}
 		}
 	}
 
-
-	/// Clears out any existing inventory UI items
-	public void ClearInventory()
-	{
-		
-		if (scrollViewContent == null)
-		{
-			scrollViewContent = transform.Find("Scroll View/Viewport/Content");
-		}
-
-		foreach (Transform child in scrollViewContent)
-		{
-			Destroy(child.gameObject);
-		}
-	}
 
 }
