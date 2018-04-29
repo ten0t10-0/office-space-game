bc21e3733904a7ecde856d0fd8b35fbd99163ffb
B <36201345+ten0t10-0@users.noreply.github.com>
2018-04-29 21:32:21 +0200

    Shop possible update

---
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ItemContainerScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemContainerScript : MonoBehaviour
+{
+    public int SupplierIndex;
+    public int InventoryItemIndex;
+}
--- Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
@@ -22,6 +22,7 @@ public class ShopController : MonoBehaviour
 	{
 		ClearInventory();
 
+        /*
         foreach (SupplierAI s in GameMaster.Instance.SupplierManager.Suppliers)
         {
 			foreach (InventoryItem item in s.Inventory.Items)
@@ -36,6 +37,30 @@ public class ShopController : MonoBehaviour
 				newItem.transform.Find("Button").GetComponent<Button>().onClick.AddListener(BuyOnClick);
 			}
 		}
+        */
+
+        int iSupplier, iInventoryItem;
+
+        for (iSupplier = 0; iSupplier < GameMaster.Instance.SupplierManager.Suppliers.Count; iSupplier++)
+        {
+            SupplierAI supplier = GameMaster.Instance.SupplierManager.Suppliers[iSupplier];
+
+            for (iInventoryItem = 0; iInventoryItem < supplier.Inventory.Items.Count; iInventoryItem++)
+            {
+                InventoryItem item = supplier.Inventory.Items[iInventoryItem];
+
+                GameObject newItem = Instantiate(ItemContainer, scrollViewContent);
+
+                newItem.GetComponent<ItemContainerScript>().SupplierIndex = iSupplier;
+                newItem.GetComponent<ItemContainerScript>().InventoryItemIndex = iInventoryItem;
+
+                newItem.transform.Find("Image").GetComponent<Image>().sprite = item.GetItemSO().Picture;
+                newItem.transform.Find("Name").GetComponent<TMP_Text>().text = item.GetItemSO().Name.ToString();
+                newItem.transform.Find("Price").GetComponent<TMP_Text>().text = item.GetItemSO().UnitCost.ToString();
+
+                newItem.transform.Find("Button").GetComponent<Button>().onClick.AddListener(BuyOnClick);
+            }
+        }
 	}
 
 	/// Clears out any existing shop UI items
@@ -56,10 +81,19 @@ public class ShopController : MonoBehaviour
 	public void BuyOnClick()
 	{
 
-		InventoryItem purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[0].Inventory.Items.Find(x => x.GetItemSO().Name.Equals(EventSystem.current.currentSelectedGameObject.transform.parent.Find("Name").GetComponent<TMP_Text>().text));
+        //InventoryItem purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[0].Inventory.Items.Find(x => x.GetItemSO().Name.Equals(EventSystem.current.currentSelectedGameObject.transform.parent.Find("Name").GetComponent<TMP_Text>().text));
 
-		//Make sure we can find the item and a can afford it
-		if (purchasedItem == null)
+        int iSupplier, iInventoryItem;
+
+        ItemContainerScript selected = EventSystem.current.currentSelectedGameObject.GetComponentInParent<ItemContainerScript>();
+
+        iSupplier = selected.GetComponent<ItemContainerScript>().SupplierIndex;
+        iInventoryItem = selected.GetComponent<ItemContainerScript>().InventoryItemIndex;
+
+        InventoryItem purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[iSupplier].Inventory.Items[iInventoryItem];
+
+        //Make sure we can find the item and a can afford it
+        if (purchasedItem == null)
 		{
 			Debug.Log("Unable to find item");
 			return;
@@ -69,5 +103,9 @@ public class ShopController : MonoBehaviour
 			Debug.Log(string.Format("Not enough monies. Purchase Price: {0}; Player Moneyz: {1}", purchasedItem.GetItemSO().UnitCost.ToString(), GameMaster.Instance.Player.Money.ToString()));
 			return;
 		}
-	}
+        else
+        {
+            Debug.Log(string.Format("Purchase info: Item Name: {0}; Purchase Price: {1}; Player remaining Moneyz: {2}", purchasedItem.GetItemSO().Name, purchasedItem.GetItemSO().UnitCost, GameMaster.Instance.Player.Money - purchasedItem.GetItemSO().UnitCost));
+        }
+    }
 }
