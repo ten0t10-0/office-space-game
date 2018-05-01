e6a0800388fcea1cc2f4d4fde81b465f9d28180b
B <36201345+ten0t10-0@users.noreply.github.com>
2018-05-01 21:12:50 +0200

    Improved/simplified camera target script
    -Camera points to base position of the player + the y offest (can be changed)

--- Office Space/Assets/Scripts/Player/PlayerTarget.cs
+++ Office Space/Assets/Scripts/Player/PlayerTarget.cs
@@ -4,24 +4,16 @@ using UnityEngine;
 
 public class PlayerTarget : MonoBehaviour
 {
-    //"targetRoot" is the actual object the camera will be targeting, but it will ignore its y position and point towards the initial local y position of "target" instead.
-    //This is done so that the camera can technically point towards "target" but not point to its realtime position.
-    //Eg. if we make "target" the head, and always point straight towards its realtime position, the camera will move up and down when you walk around (which can be irritating, motion sickness?)
+    //The position of "target" acts as the starting point for where the camera will point to.
+    //The Offset value is added to the position of the target so that the camera's position (for now, height only) is adjusted.
 
-    public GameObject targetRoot;
-    public GameObject target;
-
-    private float offsetYFromRoot;
-
-    private void Start()
-    {
-        //Gets the distance between "targetRoot" y position & "target" y position. This will get the initial local y position of "target".
-        offsetYFromRoot = target.transform.position.y - targetRoot.transform.position.y;
-    }
+    public Transform target;
+    [Range(2.25f, 3.75f)]
+    public float yOffset = 2.5f;
 
     private void LateUpdate()
     {
-        //Places the target above the Root bone by adding the distance calculated in Start() to the Y position.
-        transform.position = targetRoot.transform.position + new Vector3(0, offsetYFromRoot, 0);
+        //Places the target above the Root bone by adding the Y Offset.
+        transform.position = target.position + new Vector3(0, yOffset, 0);
     }
 }
