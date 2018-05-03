becbb7309dc070d1a0b77f658063da1daaadc881
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-05-03 19:47:17 +0200

    Buy Items

---
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/PlayerUiController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerUiController : MonoBehaviour 
+{
+	private ShopController Sc;
+
+	public InputField amount;
+	public Button btnDecrease; 
+	public Button btnIncrease;
+	public TextMeshProUGUI totalText;
+	public TextMeshProUGUI nameText;
+	//public Image pic;
+
+	public int max = 25;
+	public int min = 1;
+	float total = 0;
+	int increasePerClick = 1;
+
+	int currentAmount = 1;
+
+	Item purchasedItem;
+
+	void Start()
+	{
+		//disable decrease btn on start up
+
+		GameObject sp = GameObject.Find("StockPanel");
+		ShopController sc = sp.GetComponent<ShopController>();
+		Item purchasedItem = sc.purchasedItem;
+
+		nameText.SetText(purchasedItem.GetItemSO ().name.ToString());
+		//pic = purchasedItem.GetItemSO ().Picture;
+		totalText.SetText(purchasedItem.GetItemSO ().UnitCost.ToString());
+
+		Debug.Log("booopl"+purchasedItem.ToString ());
+
+	}
+
+	public void setItem(Item pi)
+	{
+		purchasedItem = pi;
+	}
+
+	public void InputChanged(InputField at)
+	{
+		amount.text=at.ToString();
+	}	
+
+	// ChangeAmout(true) >> increase or ChangeAmount(false) >> decrease
+	public void ChangeAmount(bool increase)
+	{
+		// clamp current value between min-max
+		currentAmount = Mathf.Clamp(currentAmount + (increase ? increasePerClick : -increasePerClick), min, max);
+		amount.text = currentAmount.ToString();
+
+		total = float.Parse(amount.text) * purchasedItem.GetItemSO().UnitCost;
+		totalText.SetText(total.ToString());
+
+		// disable buttons i
+		btnDecrease.interactable = currentAmount > min;
+		btnIncrease.interactable = currentAmount < max;
+	}
+
+
+
+	public void BuyItemOnClick()
+	{
+		//deduct the money from the player
+
+
+		//Update the balance in ui Somewhere
+
+		//add the item to the players inventory
+
+		//decrease the players inventory space
+
+		//deactivate panel
+
+
+	}
+
+}
--- Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/ShopController.cs
@@ -12,7 +12,12 @@ public class ShopController : MonoBehaviour
 	private GameObject ItemContainer;
 	private Transform scrollViewContent;
 
-	
+	public GameObject qtyPanel;
+	private PlayerUiController playerUI;
+
+	[HideInInspector]
+	public Item purchasedItem;
+
 	void Start () 
 	{
 		AddAllItems();
@@ -21,23 +26,6 @@ public class ShopController : MonoBehaviour
 	public void AddAllItems()
 	{
 		ClearInventory();
-
-        /*
-        foreach (SupplierAI s in GameMaster.Instance.SupplierManager.Suppliers)
-        {
-			foreach (InventoryItem item in s.Inventory.Items)
-			{
-				GameObject newItem = Instantiate(ItemContainer,scrollViewContent);
-
-				newItem.transform.Find("Image").GetComponent<Image> ().sprite = item.GetItemSO().Picture;
-				newItem.transform.Find("Name").GetComponent<TMP_Text>().text = item.GetItemSO().Name.ToString();
-				newItem.transform.Find("Price").GetComponent<TMP_Text>().text = item.GetItemSO().UnitCost.ToString();
-
-				//checks if button pressed
-				newItem.transform.Find("Button").GetComponent<Button>().onClick.AddListener(BuyOnClick);
-			}
-		}
-        */
 
         int iSupplier, iItem;
 
@@ -80,9 +68,6 @@ public class ShopController : MonoBehaviour
 
 	public void BuyOnClick()
 	{
-
-        //InventoryItem purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[0].Inventory.Items.Find(x => x.GetItemSO().Name.Equals(EventSystem.current.currentSelectedGameObject.transform.parent.Find("Name").GetComponent<TMP_Text>().text));
-
         int iSupplier, iInventoryItem;
 
         ItemContainerScript selected = EventSystem.current.currentSelectedGameObject.GetComponentInParent<ItemContainerScript>();
@@ -90,7 +75,7 @@ public class ShopController : MonoBehaviour
         iSupplier = selected.GetComponent<ItemContainerScript>().SupplierIndex;
         iInventoryItem = selected.GetComponent<ItemContainerScript>().InventoryItemIndex;
 
-        Item purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[iSupplier].Inventory.Items[iInventoryItem];
+		purchasedItem = GameMaster.Instance.SupplierManager.Suppliers[iSupplier].Inventory.Items[iInventoryItem];
 
         //Make sure we can find the item and a can afford it
         if (purchasedItem == null)
@@ -107,5 +92,10 @@ public class ShopController : MonoBehaviour
         {
             Debug.Log(string.Format("Purchase info: Item Name: {0}; Purchase Price: {1}; Player remaining Moneyz: {2}", purchasedItem.GetItemSO().Name, purchasedItem.GetItemSO().UnitCost, GameMaster.Instance.Player.Money - purchasedItem.GetItemSO().UnitCost));
         }
+			
+		qtyPanel.SetActive(true);
+
     }
+
+
 }
