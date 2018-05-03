2b7c5e47343700d3ed674fa8436d2f3186b8d9e6
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-05-04 00:12:04 +0200

    add UI asset pack

---
+++ Office Space/Assets/Ui Assets/Modern UI Pack/Scripts/Dropdown.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Dropdown : MonoBehaviour {
+
+	[Header("ANIMATORS")]
+	public Animator dropdownAnimator;
+
+	[Header("OBJECTS")]
+	public GameObject fieldTrigger;
+	public Text selectedText;
+	public Image selectedImage;
+
+	[Header("SETTINGS")]
+	[Tooltip("IMPORTANT! EVERY DORPDOWN MUST HAVE A DIFFERENT ID")]
+	public string DropdownID = "0";
+
+	//public bool darkTrigger = true;
+
+	private bool isOn;
+	private string inAnim = "In";
+	private string outAnim = "Out";
+
+	private string sText;
+	private string sImage;
+
+	void Start ()
+	{
+		sText = PlayerPrefs.GetString (DropdownID + "SelectedText");
+		sImage = PlayerPrefs.GetString (DropdownID + "SelectedImage");
+
+		selectedText.text = sText;
+	//	selectedImage.sprite = 
+	}
+
+	public void Animate ()
+	{
+		if (isOn == true) 
+		{
+			dropdownAnimator.Play (outAnim);
+			isOn = false;
+			fieldTrigger.SetActive (false);
+		}
+
+		else
+		{
+			dropdownAnimator.Play (inAnim);
+			isOn = true;
+			fieldTrigger.SetActive (true);
+		}
+	}
+}
