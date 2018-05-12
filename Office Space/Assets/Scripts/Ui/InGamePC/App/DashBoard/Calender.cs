bbcddb769519480f9dc9499fa265f35d70076860
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-05-13 01:31:27 +0200

    Calender Working

--- Office Space/Assets/Scripts/Ui/InGamePC/App/DashBoard/Calender.cs
+++ Office Space/Assets/Scripts/Ui/InGamePC/App/DashBoard/Calender.cs
@@ -16,109 +16,110 @@ public class Calender : MonoBehaviour {
 	int day ;
 	int year;
 
+	public TextMeshProUGUI monthName;
 
-	//int daysInMonth;
-
+	public Sprite image;
 
 	void Start () 
 	{
-	   //month = GameMaster.Instance.GameDateTime.Month;
 		month = 9;
 		day = GameMaster.Instance.GameDateTime.Day;
 		year = GameMaster.Instance.GameDateTime.Year;
-
+		monthName.SetText(GameMaster.Instance.GameDateTime.ToString("MMMM"));
 
 		calculateMonth ();
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		
 	}
 
 	void calculateMonth ()
 	{
 
-//		DayOfWeek dayName = GameMaster.Instance.GameDateTime.DayOfWeek;
-//		GameObject newBlank;
-//
-//
-//		switch (dayName) {
-//		case DayOfWeek.Tuesday:
-//			
-//				newBlank = Instantiate (dayContainer, Content);
-//				newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//				break;
-//
-//		case DayOfWeek.Wednesday:
-//			
-//				for (int i = 0; i >= 1; i++) {
-//					newBlank = Instantiate (dayContainer, Content);
-//					newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//
-//				}
-//				break;
-//
-//		case DayOfWeek.Thursday:
-//			
-//				for (int i = 0; i >= 2; i++) {
-//					newBlank = Instantiate (dayContainer, Content);
-//					newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//
-//				}
-//				break;
-//
-//		case DayOfWeek.Friday:
-//			
-//				for (int i = 0; i >= 3; i++) {
-//					newBlank = Instantiate (dayContainer, Content);
-//					newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//
-//				}
-//				break;
-//
-//		case DayOfWeek.Saturday:
-//			
-//				for (int i = 0; i >= 4; i++) {
-//					newBlank = Instantiate (dayContainer, Content);
-//					newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//
-//				}
-//				break;
-//
-//		case DayOfWeek.Sunday:
-//			
-//				for (int i = 0; i >= 5; i++) {
-//					newBlank = Instantiate (dayContainer, Content);
-//					newBlank.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
-//
-//				}
-//				break;
-//			}
+		ClearCalender ();
+
+		DateTime dt = new DateTime(year, month, 1);
+		GameObject newItem;
+
+		switch (dt.DayOfWeek) 
+		{
+		case DayOfWeek.Tuesday:
+			
+			newItem = Instantiate (dayContainer, Content);
+			newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+				break;
+
+		case DayOfWeek.Wednesday:
+			
+				for (int i = 0; i <= 1; i++) 
+				{
+				newItem = Instantiate (dayContainer, Content);
+				newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+
+				}
+				break;
+
+		case DayOfWeek.Thursday:
+			
+				for (int i = 0; i <= 2; i++) 
+				{
+					newItem = Instantiate (dayContainer, Content);
+					newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+
+				}
+				break;
+
+		case DayOfWeek.Friday:
+			
+				for (int i = 0; i <= 3; i++) 
+				{
+					newItem = Instantiate (dayContainer, Content);
+					newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+
+				}
+				break;
+
+		case DayOfWeek.Saturday:
+			
+				for (int i = 0; i <= 4; i++) 
+				{
+					newItem = Instantiate (dayContainer, Content);
+					newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+
+				}
+				break;
+
+		case DayOfWeek.Sunday:
+			
+				for (int i = 0; i <= 5; i++) 
+				{
+					newItem = Instantiate (dayContainer, Content);
+					newItem.transform.Find ("day").GetComponent<TMP_Text> ().text = "";
+
+				}
+				break;
+		}
 
 		
 
 
 		for (var date = new DateTime (year, month, 1); date.Month == month; date = date.AddDays (1))
 		{
-			
-			GameObject newItem = Instantiate(dayContainer, Content);
+			newItem = Instantiate(dayContainer, Content);
 
 			newItem.transform.Find("day").GetComponent<TMP_Text>().text = date.Day.ToString();
 
-			//if (date.Day == day) 
-
-				//var panel = newItem.transform.Find ("Colour");
-
-			
-		
-
-
+			if (date.Day == day) 
+			{
+				newItem.transform.Find ("Image").GetComponent<Image> ().sprite = image;;
+			}
 		}
 
 
+	}
+
+	public void ClearCalender()
+	{
+		foreach (Transform child in Content)
+		{
+			Destroy(child.gameObject);
+		}
 	}
 }
