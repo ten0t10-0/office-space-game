f65f1a36a4594831030f0112a2d86cbd08e69d8f
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-07-19 21:33:57 +0200

    Walk in closet

---
+++ Office Space/Assets/Scripts/DayNightController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class DayNightController : MonoBehaviour 
+{
+
+    public Light sun;
+
+    [Range(0,1)]
+	public float currentTimeOfDay = 0;
+	float time;
+
+    float sunInitialIntensity;
+    void Start() 
+	{
+        sunInitialIntensity = sun.intensity;
+    }
+
+    void Update() 
+	{
+
+        UpdateSun();
+
+		time = (Convert.ToSingle(GameMaster.Instance.GameDateTime.Hour)) + (Convert.ToSingle(GameMaster.Instance.GameDateTime.Minute) / 100);
+		currentTimeOfDay = time / 24;
+	
+        if (currentTimeOfDay >= 1) {
+            currentTimeOfDay = 0;
+        }
+    }
+
+    void UpdateSun() 
+	{
+
+        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+	
+        float intensityMultiplier = 1;
+
+        if (currentTimeOfDay <= 0.23f || currentTimeOfDay >= 0.75f) 
+		{
+            intensityMultiplier = 0;
+        }
+
+        else if (currentTimeOfDay <= 0.25f) 
+		{
+
+            intensityMultiplier = Mathf.Clamp01((currentTimeOfDay - 0.23f) * (1 / 0.02f));
+        }
+
+        else if (currentTimeOfDay >= 0.73f) 
+		{
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTimeOfDay - 0.73f) * (1 / 0.02f)));
+        }
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+}
---
+++ Office Space/Assets/Scripts/Office/MirrorTrigger.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorTrigger : MonoBehaviour {
+
+	public GameObject mirror;
+	private bool isInsideTrigger = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (isInsideTrigger == true) 
+		{
+			mirror.SetActive (true);
+		}
+		if (isInsideTrigger == false) 
+		{
+			mirror.SetActive (false);
+		}
+	}
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			isInsideTrigger = true;
+
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "Player")
+		{
+			isInsideTrigger = false;
+
+		}
+	}
+}
