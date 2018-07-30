11e5ccc52638eff84497ab7e84feb6d0102ce665
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-07-30 21:16:12 +0200

    Sound v1.0

---
+++ Office Space/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour 
+{
+	public SoundClass[] sounds;
+
+	void Awake () 
+	{
+		foreach (SoundClass s in sounds) 
+		{
+			s.source = gameObject.AddComponent<AudioSource> ();
+
+			s.source.clip = s.clip;
+
+			s.source.volume = s.volume;
+			s.source.pitch = s.pitch;
+
+			s.source.loop = s.loop;
+		}
+
+	}
+	void Start()
+	{
+		Play ("Theme");
+	}
+	
+	public void Play(string name)
+	{
+		SoundClass s = Array.Find (sounds, sound => sound.name == name);
+
+		s.source.Play ();
+	}
+
+}
