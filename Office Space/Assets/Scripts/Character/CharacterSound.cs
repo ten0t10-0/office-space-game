c14f142ac1fb8ed916942cbbee611f04d1cb6fd9
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-08-10 19:48:48 +0200

    Footstep sounds

---
+++ Office Space/Assets/Scripts/Character/CharacterSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSound : MonoBehaviour 
+{
+
+	[SerializeField]
+	private AudioClip[] clips;
+
+	AudioSource audioSource;
+
+	// Use this for initialization
+	private void Awake () 
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	private void Step()
+	{
+		AudioClip clip = GetRandomClip ();
+		audioSource.PlayOneShot(clip);	
+	}
+
+	private AudioClip GetRandomClip()
+	{
+		return clips [UnityEngine.Random.Range (0, clips.Length)];
+	}
+}
