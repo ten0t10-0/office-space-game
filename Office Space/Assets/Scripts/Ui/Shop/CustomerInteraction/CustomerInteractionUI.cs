f41d4fcc47071d5fc4f0d2ef8a9d07e97550d435
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-08-31 18:36:36 +0200

    Saftey

---
+++ Office Space/Assets/Scripts/Ui/Shop/CustomerInteraction/CustomerInteractionUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerInteractionUI : MonoBehaviour 
+{
+
+	public Animator customer,player,bars,item;
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+
+}
