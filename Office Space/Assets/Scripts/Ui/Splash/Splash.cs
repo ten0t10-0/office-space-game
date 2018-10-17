6da7fd05992de23ae7049fe560231bf4829e97e1
hobo24 <37043776+hobo24@users.noreply.github.com>
2018-10-17 16:02:23 +0200

    splash screen

--- Office Space/Assets/Scripts/Ui/Splash/Splash.cs
+++ Office Space/Assets/Scripts/Ui/Splash/Splash.cs
@@ -6,31 +6,32 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour
 {
-    public Image splashImage;
+  //  public Image splashImage;
     public string loadMenu;
 
     IEnumerator Start()
     {
-        splashImage.canvasRenderer.SetAlpha(0.0f);
+       // splashImage.canvasRenderer.SetAlpha(0.0f);
 
-        FadeIn();
-        yield return new WaitForSeconds(2.5f);
-        FadeOut();
-        yield return new WaitForSeconds(2.5f);
+//        FadeIn();
+//        yield return new WaitForSeconds(2.5f);
+//        FadeOut();
+//        yield return new WaitForSeconds(2.5f);
+		yield return new WaitForSeconds(3.3f);
 
         SceneManager.LoadScene(loadMenu);
     }
 
-    void FadeIn()
-    {
-        splashImage.CrossFadeAlpha(1.0f,1.5f,false);
-    }
-
-    void FadeOut()
-    {
-        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
-    }
-
+//    void FadeIn()
+//    {
+//        splashImage.CrossFadeAlpha(1.0f,1.5f,false);
+//    }
+//
+//    void FadeOut()
+//    {
+//        splashImage.CrossFadeAlpha(0.0f, 2.5f, false);
+//    }
+//
 
 
 }
